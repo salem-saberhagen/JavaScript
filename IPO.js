519fb0b8ec038a48541676d20872caefc0441011
salem-saberhagen <davincitovar@gmail.com>
2020-01-29 22:41:07 -0600

    challenge2

--- IPO.js
+++ IPO.js
@@ -21,30 +21,6 @@ console.log("El mínimo: " + Math.min(9, 10, 30, 2, 0));
 console.log("El máximo: " + Math.max(9, 10, 30, 2, 0));
 console.log("2 elevado a la 4a: " + Math.pow(2, 4));
 
-//Challenge 2
-
-// 1. Input, processing, output.
-
-// Esto es un comentario
-/* Este también es
-un comentario */
-
-// Imprimiendo en la consola
-console.log("Hola, Mundo");
-
-// Operaciones Aritméticas
-console.log("Sumar 4 + 2: " + (4 + 2));
-console.log("Restar 9 - 3: " + (9 - 3));
-console.log("Multiplicar 2 * 3: " + (2 * 3));
-console.log("Dividir 12 / 2: " + (12 / 2));
-
-// Operador Reminder
-console.log("El residuo de 13 % 2: " + (13 % 2));
-
-// Funciones Matemáticas
-console.log("El mínimo: " + Math.min(9, 10, 30, 2, 0));
-console.log("El máximo: " + Math.max(9, 10, 30, 2, 0));
-console.log("2 elevado a la 4a: " + Math.pow(2, 4));
 
 //challenge 2
 
